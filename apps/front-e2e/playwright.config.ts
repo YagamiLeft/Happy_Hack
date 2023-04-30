06d37556c71c24f4e1e4c07a5cbb272b0158197d
YagamiLeft <yagamileft3@gmail.com>
2023-04-30 15:07:20 +0900

    add playwright

---
+++ apps/front-e2e/playwright.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src/e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+
+  webServer: {
+    command: 'nx serve front --port=5200',
+    port: 5200,
+  },
+});
